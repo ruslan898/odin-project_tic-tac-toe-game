fdeccedc85e81e688447b324e2a6cdf4a8398dc7
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2025-09-22 20:04:09 +0500

    Complete tic-tac-toe project

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const Gameboard = (function () {
-  const gameboard = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+  const gameboard = ['', '', '', '', '', '', '', '', ''];
 
   const getGameboard = () => [...gameboard];
 
-  let emptyCells = gameboard.length;
+  let emptyCells = gameboard.reduce((count, cell) => {
+    return !cell ? ++count : count;
+  }, 0);
+
+  const gameboardCells = document.querySelectorAll('.cell');
+
+  const updateEmptyCells = () => {
+    emptyCells = gameboard.reduce((count, cell) => {
+      return !cell ? ++count : count;
+    }, 0);
+  };
 
   /**
    *
@@ -13,37 +23,37 @@ const Gameboard = (function () {
   function fillGameboardCell(cellNum, marker) {
     if (
       cellNum <= gameboard.length &&
-      gameboard[cellNum - 1] === '-' &&
+      gameboard[cellNum] === '' &&
       emptyCells > 0
     ) {
-      gameboard[cellNum - 1] = marker;
-      emptyCells--;
+      gameboard[cellNum] = marker;
+      updateEmptyCells();
     }
   }
 
   const getEmptyCells = () => emptyCells;
 
   function renderGameboard() {
-    let currentRow = '';
     gameboard.forEach((cell, i) => {
-      currentRow += cell + ' ';
-
-      if (i === 0 || i === 1) {
-        return;
-      }
-
-      if ((i + 1) % 3 === 0) {
-        currentRow += '\n';
-      }
+      gameboardCells[i].textContent = cell;
     });
-
-    console.log(currentRow);
   }
 
-  return { getGameboard, fillGameboardCell, renderGameboard, getEmptyCells };
-})();
+  function clearGameboard() {
+    for (let i = 0; i < gameboard.length; i++) {
+      gameboard[i] = '';
+    }
+    updateEmptyCells();
+  }
 
-Gameboard.renderGameboard();
+  return {
+    getGameboard,
+    fillGameboardCell,
+    renderGameboard,
+    getEmptyCells,
+    clearGameboard,
+  };
+})();
 
 function Player(name, marker, isActive) {
   let isActivePlayer = isActive;
@@ -54,11 +64,32 @@ function Player(name, marker, isActive) {
     isActivePlayer = !isActivePlayer;
   };
 
+  function clearNames() {
+    playerOne.changeName('Player One');
+    playerTwo.changeName('Player Two');
+  }
+
+  const clearActiveStatus = () => {
+    if (playerTwo.getActiveStatus()) {
+      playerTwo.toggleActiveStatus();
+      playerOne.toggleActiveStatus();
+    }
+  };
+
+  const getName = () => name;
+
+  const changeName = (newName) => (name = newName);
+
+  const getMarker = () => marker;
+
   return {
-    name,
-    marker,
     getActiveStatus,
     toggleActiveStatus,
+    getName,
+    changeName,
+    getMarker,
+    clearNames,
+    clearActiveStatus,
   };
 }
 
@@ -79,10 +110,9 @@ const GameController = (function () {
 
   let isFinished = false;
 
-  // const getPlayerInput = (marker) => {
-  //   const cellNum = +prompt('Please enter a number from 1 to 9');
-  //   return { cellNum, marker };
-  // };
+  const getFinishedStatus = () => isFinished;
+
+  const resetFinishedStatus = () => (isFinished = false);
 
   function checkWin() {
     let isWin = false;
@@ -116,11 +146,11 @@ const GameController = (function () {
     const [playerOneMarker, playerTwoMarker] = playerMarkers;
 
     if (winMarker === playerOneMarker) {
-      console.log(`${playerOne.name} wins!`);
+      return `${playerOne.getName()} wins!`;
     } else if (winMarker === playerTwoMarker) {
-      console.log(`${playerTwo.name} wins!`);
+      return `${playerTwo.getName()} wins!`;
     } else {
-      console.log("It's a draw!");
+      return "It's a draw!";
     }
   }
 
@@ -141,27 +171,126 @@ const GameController = (function () {
     });
   }
 
-  function playRound() {
+  function playRound(index) {
     const activePlayer = getActivePlayer([playerOne, playerTwo]);
-    const playerInput = getPlayerInput(activePlayer.marker);
-    Gameboard.fillGameboardCell(playerInput.cellNum, playerInput.marker);
+    const playerInput = activePlayer.getMarker();
+    Gameboard.fillGameboardCell(index, playerInput);
     Gameboard.renderGameboard();
     toggleActivePlayer([playerOne, playerTwo]);
   }
 
-  function playGame() {
-    while (!isFinished) {
-      if (checkWin().isWin === true || Gameboard.getEmptyCells() <= 0) {
-        isFinished = true;
-        break;
-      }
-      playRound();
+  function endGame() {
+    if (checkWin().isWin === true || Gameboard.getEmptyCells() <= 0) {
+      isFinished = true;
+      const result = declareResult(checkWin().winMarker, [
+        playerOne.getMarker(),
+        playerTwo.getMarker(),
+      ]);
+      Display.displayResult(result);
     }
-
-    declareResult(checkWin().winMarker, [playerOne.marker, playerTwo.marker]);
   }
 
-  return { playGame };
+  return {
+    endGame,
+    getActivePlayer,
+    declareResult,
+    playRound,
+    getFinishedStatus,
+    resetFinishedStatus,
+  };
 })();
 
-GameController.playGame();
+const Display = (function () {
+  const playerNames = document.querySelectorAll('.player-name');
+  const playerNameSpans = document.querySelectorAll('.player-name span');
+  const playerInputs = document.querySelectorAll('.player-name-input');
+  const btnStart = document.querySelector('#btn-start');
+  const btnRestart = document.querySelector('#btn-restart');
+  const gameboard = document.querySelector('.gameboard');
+  const gameboardCells = document.querySelectorAll('.cell');
+  const playerTurn = document.querySelector('.player-turn');
+  const gameResultEl = document.querySelector('.game-result');
+
+  function hideElements(...elems) {
+    elems.forEach((elem) => elem.classList.add('hide'));
+  }
+  function showElements(...elems) {
+    elems.forEach((elem) => elem.classList.remove('hide'));
+  }
+  function displayPlayerTurn() {
+    const activePlayer = GameController.getActivePlayer([playerOne, playerTwo]);
+    playerTurn.textContent = `${activePlayer.getName()}'s turn`;
+  }
+  function displayResult(result) {
+    gameResultEl.textContent = result;
+    showElements(gameResultEl, btnRestart);
+    hideElements(playerTurn);
+  }
+  function clearInputs() {
+    playerInputs.forEach((el) => {
+      const input = el.querySelector('input');
+      input.value = '';
+    });
+  }
+  function clearTextContent(...elems) {
+    elems.forEach((elem) => (elem.textContent = ''));
+  }
+
+  playerInputs.forEach((input) => {
+    input.addEventListener('click', function (e) {
+      if (e.target.classList.contains('btn')) {
+        const input = this.querySelector('input');
+        const inputNum = input.dataset.nameNum;
+        const inputValue = input.value;
+        if (inputValue) {
+          showElements(playerNames[inputNum - 1]);
+          playerNameSpans[inputNum - 1].textContent = inputValue;
+          if (inputNum === '1') {
+            playerOne.changeName(inputValue);
+          } else {
+            playerTwo.changeName(inputValue);
+          }
+          hideElements(this);
+        }
+      }
+    });
+  });
+
+  btnStart.addEventListener('click', function () {
+    const haveNames = [...playerNameSpans].every((span) => span.textContent);
+    if (haveNames) {
+      hideElements(this);
+      showElements(gameboard, playerTurn);
+      displayPlayerTurn();
+    }
+  });
+
+  gameboardCells.forEach(function (cell, index) {
+    cell.addEventListener('click', function () {
+      if (!GameController.getFinishedStatus()) {
+        GameController.playRound(index);
+        displayPlayerTurn();
+        GameController.endGame();
+      }
+    });
+  });
+
+  btnRestart.addEventListener('click', function () {
+    Gameboard.clearGameboard();
+    playerOne.clearNames();
+    playerOne.clearActiveStatus();
+    GameController.resetFinishedStatus();
+    clearInputs();
+    hideElements(
+      ...playerNames,
+      btnRestart,
+      gameboard,
+      playerTurn,
+      gameResultEl
+    );
+    showElements(...playerInputs, btnStart);
+    clearTextContent(...playerNameSpans, ...gameboardCells);
+  });
+
+  return { displayResult };
+})();
